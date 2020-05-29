2511c9c818f12cbb7057cc9677bc680ea7d5808d
orby-tech <tibode495@gmail.com>
2020-05-29 21:20:07 +0300

    seid hi!)

---
+++ firstNativeModule/build/Release/.deps/Release/hello_world.node.d
@@ -0,0 +1,1 @@
+cmd_Release/hello_world.node := rm -rf "Release/hello_world.node" && cp -af "Release/obj.target/hello_world.node" "Release/hello_world.node"
---
+++ firstNativeModule/build/Release/.deps/Release/obj.target/hello_world/hello_world.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/hello_world/hello_world.o := g++ '-DNODE_GYP_MODULE_NAME=hello_world' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/oem/.cache/node-gyp/10.20.1/include/node -I/home/oem/.cache/node-gyp/10.20.1/src -I/home/oem/.cache/node-gyp/10.20.1/deps/openssl/config -I/home/oem/.cache/node-gyp/10.20.1/deps/openssl/openssl/include -I/home/oem/.cache/node-gyp/10.20.1/deps/uv/include -I/home/oem/.cache/node-gyp/10.20.1/deps/zlib -I/home/oem/.cache/node-gyp/10.20.1/deps/v8/include -I/home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/hello_world/hello_world.o.d.raw   -c -o Release/obj.target/hello_world/hello_world.o ../hello_world.cc
+Release/obj.target/hello_world/hello_world.o: ../hello_world.cc \
+ /home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi.h \
+ /home/oem/.cache/node-gyp/10.20.1/include/node/node_api.h \
+ /home/oem/.cache/node-gyp/10.20.1/include/node/node_api_types.h \
+ /home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi-inl.h \
+ /home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi-inl.deprecated.h
+../hello_world.cc:
+/home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi.h:
+/home/oem/.cache/node-gyp/10.20.1/include/node/node_api.h:
+/home/oem/.cache/node-gyp/10.20.1/include/node/node_api_types.h:
+/home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi-inl.h:
+/home/oem/cppmoduls/firstNativeModule/node_modules/node-addon-api/napi-inl.deprecated.h:
